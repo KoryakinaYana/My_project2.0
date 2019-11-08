1e55c80f62d856fb532b294035e57830d1e1df39
KoryakimaYana <yana.kukushkina.1981@gmail.com>
2019-11-08 18:15:15 +0700

    triangle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from turtle import *
+from math import sqrt
 #Geraschenko Maria - def(romb)
 #Koryakina Yana - def(triangle)
 #LeontevaSofia - def(square)
@@ -6,5 +7,15 @@ from turtle import *
 #Maria - romb
 
 #Yana - triangle
-
+def triangle (x,color1):
+    fillcolor(color1)
+    begin_fill()
+    color(color1)
+    forward (x)
+    left (90)
+    forward (x)
+    left (135)
+    forward (sqrt(2*x**2))
+    end_fill()
+    mainloop()
 #Sonya - square
