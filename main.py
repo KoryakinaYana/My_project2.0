d3231d9406b3802404fc46971ed8fef256adc224
masha200 <masha20042014@ngs.ru>
2019-11-08 18:37:32 +0700

    #hashtag

--- main.py
+++ main.py
@@ -4,7 +4,19 @@ from turtle import *
 #LeontevaSofia - def(square)
 
 #Maria - romb
-
+def romb(x, color1):
+    down()
+    color(color1)
+    begin_fill()
+    for _ in range (2):
+        forward(x)
+        right(135)
+        forward(x)
+        right(45)
+    end_fill()
+    up()
+    mainloop()
+romb(100, 'red')
 #Yana - triangle
 
 #Sonya - square
